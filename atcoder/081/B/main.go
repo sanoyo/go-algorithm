5bd2e36d5eb1a5b4a9990b01407d3c2c23f1676d
sanoyo <sa00380304yo@gmail.com>
2020-01-05 19:15:33 +0900

    コメント追加

--- atcoder/081/B/main.go
+++ atcoder/081/B/main.go
@@ -10,12 +10,15 @@ func main() {
 		count int
 	)
 	fmt.Scan(&n)
+	// N個分のスライスを定義
 	nums := make([]int, n)
+	// N回文数値を受けとる
 	for i := 0; i < n; i++ {
 		fmt.Scan(&nums[i])
 	}
  
 	flag := true
+	// flagがtrueだったら、for分が回り続ける
 	for flag {
 		for i := 0; i < n; i++ {
 			if nums[i]%2 == 0 {
