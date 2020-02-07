787d9cd3bf4eaa59d3af0c0a8e4e47a681f22ec5
sanoyo <sa00380304yo@gmail.com>
2020-02-07 09:02:56 +0900

    109 C問題修正

--- atcoder/109/C/001_main.go
+++ atcoder/109/C/001_main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"sort"
+
+	// "sort"
 	"strconv"
 )
 
@@ -12,13 +14,6 @@ func nextInt(sc *bufio.Scanner) int {
 	sc.Scan()
 	t, _ := strconv.Atoi(sc.Text())
 	return t
-}
-
-func gcd(a, b int) int {
-	if a%b == 0 {
-		return b
-	}
-	return gcd(b, a%b)
 }
 
 func main() {
@@ -32,23 +27,26 @@ func main() {
 		A[i] = nextInt(sc)
 	}
 
-	A = append(A, X)
+	A = append(A, X) //Xを追加
 	sort.Ints(A)
-	fmt.Println(A)
-
 	intervals := make([]int, 0)
-	// 0番目をスキップするように実装
 	for i := 1; i < len(A); i++ {
-		fmt.Println(i)
 		intervals = append(intervals, A[i]-A[i-1])
 	}
-
 	fmt.Println(intervals)
+	fmt.Println("-----")
 
+	// intervalsの最初の値
 	ans := intervals[0]
-	fmt.Println(ans)
-	for i := 0; i < len(intervals); i++ {
+	for i := 1; i < len(intervals); i++ {
 		ans = gcd(ans, intervals[i])
 	}
 	fmt.Println(ans)
+}
+
+func gcd(a, b int) int {
+	if a%b == 0 {
+		return b
+	}
+	return gcd(b, a%b)
 }
