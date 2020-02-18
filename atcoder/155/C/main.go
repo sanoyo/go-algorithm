728bb4acc38c6a967456e17c406f4de9f4e42702
sanoyo <sa00380304yo@gmail.com>
2020-02-18 09:05:35 +0900

    155C問題復習

--- atcoder/155/C/main.go
+++ atcoder/155/C/main.go
@@ -1,60 +1,43 @@
 package main
- 
+
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"sort"
-	"strconv"
 )
- 
-func solution(n int, texts []string) []string {
-	ans := make([]string, 0)
-	var c int
-	seen := make(map[string]int, n)
- 
-	// mapで同じものをカウント
-	// map[beat:1 bed:1 beet:2 bet:1 vet:2]
-	for _, text := range texts {
-		seen[text]++
-		c = max(c, seen[text])
-	}
- 
-	for k, v := range seen {
-		// value(数値)とmaxのカウント数が同じなのであれば、ansに追加
-		if v == c {
-			ans = append(ans, k)
-		}
-	}
-	sort.Strings(ans)
-	return ans
-}
- 
+
 func max(x, y int) int {
 	if x > y {
 		return x
 	}
 	return y
 }
- 
+
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanWords)
- 
-	sc.Scan()
-	// 文字列カウント
-	n, _ := strconv.Atoi(sc.Text())
- 
-	// 文字列を配列に入れる
+	var n int
+	fmt.Scan(&n)
+
 	texts := make([]string, n)
 	for i := 0; i < n; i++ {
-		sc.Scan()
-		texts[i] = sc.Text()
+		fmt.Scan(&texts[i])
 	}
- 
-	// 一番出てきた回数が多いものを残す
-	ans := solution(n, texts)
-	// 残した配列を順番に出力する
+
+	var num int
+	ans := make([]string, 0)
+	maps := make(map[string]int, n)
+	// mapで同じものをカウント
+	for i := 0; i < n; i++ {
+		maps[texts[i]]++
+		num = max(num, maps[texts[i]])
+	}
+
+	// value(数値)とmaxのカウント数が同じなのであれば、ansに追加
+	for key, value := range maps {
+		if value == num {
+			ans = append(ans, key)
+		}
+	}
+	sort.Strings(ans)
+
 	for _, text := range ans {
 		fmt.Println(text)
 	}
