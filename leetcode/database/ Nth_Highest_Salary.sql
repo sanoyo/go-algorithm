4997ce53b2bf1719415d9c471d77cf0d89698c37
sanoyo <sa00380304yo@gmail.com>
2021-04-10 16:11:11 +0900

    Nth Highest

---
+++ leetcode/database/ Nth_Highest_Salary.sql
@@ -0,0 +1,13 @@
+-- https://github.com/XiancaiTian/Leetcode-database/blob/master/Nth%20Highest%20Salary.sql
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE skip INT;
+  SET skip = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT skip,1
+  );
+END
